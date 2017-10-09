013a09b0a80594303bae0cbdfb73d846e73e7b0e
Astrea16 <user@example.com>
2017-10-09 20:22:00 +0300

    js-files

---
+++ js/scill-donor.js
@@ -0,0 +1,50 @@
+$(function() {
+	$('#modal-1 form, #modal-2 form, #form-3').submit(function(e) {
+		e.preventDefault();
+		var formValid = true;
+		$(this).find('input').each(function() {
+			var formGroup = $(this).parents('.form-group');
+			var glyphicon = formGroup.find('.form-control-feedback');
+			if (this.checkValidity()) {
+				formGroup.addClass('has-success').removeClass('has-error');
+				glyphicon.addClass('glyphicon-ok').removeClass('glyphicon-remove');
+			} else {
+				formGroup.addClass('has-error').removeClass('has-success');
+				glyphicon.addClass('glyphicon-remove').removeClass('glyphicon-ok');
+				formValid = false;  
+			}
+		});
+		if (formValid) {
+			var email = $("#input-email, #input-email-1, #input-footer").val();
+			var password = $("#input-password, #input-password-1,#input-password-3").val();
+			var checkbox = $("#check-1,#check-2").val();
+
+			var formData = new FormData();
+			formData.append('email', email);
+			formData.append('password', password);
+			formData.append('checkbox',checkbox);
+
+			$.ajax({
+				type: "POST",
+				url: "scill-donor.php",
+				data: formData,
+				contentType: false,
+				processData: false,
+				cashe: false,
+				success : function(data){
+					var $data = ISON.parse(data);
+					$('#error').text('');
+					if ($data.result == "success"){
+						$('#modal-1 form, #modal-2 form').hide();
+						$('#success-alert,#success-alert-1').removeClass('hidden')
+					}else{
+						$('#error,#error-1').text('There were errors while sending the form to the server.')
+					} 
+				},
+				error: function (request) {
+					$('#error').text('An error has occurred ' + request.responseText + ' when sending data.');
+				}     
+			});
+		}
+	});
+});
