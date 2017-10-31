2d5baf520966ee5a8ab7d5b57e7415000983e4bf
Astrea16 <user@example.com>
2017-10-31 21:35:19 +0300

    script modification

--- js/scill-donor.js
+++ js/scill-donor.js
@@ -1,4 +1,4 @@
-$(function() {
+/*$(function() {
 	$('#modal-1 form, #modal-2 form, #form-3').submit(function(e) {
 		e.preventDefault();
 		var formValid = true;
@@ -26,13 +26,13 @@ $(function() {
 
 			$.ajax({
 				type: "POST",
-				url: "scill-donor.php",
+				url: "send.php",
 				data: formData,
 				contentType: false,
 				processData: false,
 				cashe: false,
 				success : function(data){
-					var $data = ISON.parse(data);
+					var $data = JSON.parse(data);
 					$('#error').text('');
 					if ($data.result == "success"){
 						$('#modal-1 form, #modal-2 form').hide();
@@ -42,9 +42,60 @@ $(function() {
 					} 
 				},
 				error: function (request) {
-					$('#error').text('An error has occurred ' + request.responseText + ' when sending data.');
+					$('#error,#error-1').text('An error has occurred ' + request.responseText + ' when sending data.');
 				}     
 			});
+			$("#modal-1 form, #modal-2 form, #form-3").trigger('reset'), 3000;
 		}
+	});
+});*/
+$(document).ready(function () {
+	$("form").submit(function (event) {
+		event.preventDefault();
+		var formId = $(this).attr('id');
+		var formNm = $('#' + formId);
+		var formValid = true;
+		$(this).find('input').each(function() {
+			var formGroup = $(this).parents('.form-group');
+			var glyphicon = formGroup.find('.form-control-feedback');
+			if (this.checkValidity()) {
+				formGroup.addClass('has-success').removeClass('has-error');
+				glyphicon.addClass('glyphicon-ok').removeClass('glyphicon-remove');
+			} else {
+				formGroup.addClass('has-error').removeClass('has-success');
+				glyphicon.addClass('glyphicon-remove').removeClass('glyphicon-ok');
+				formValid = false;  
+			}
+		});
+		$.ajax({
+			type: "POST",
+			url: 'send.php',
+			data: formNm.serialize(),
+			success: function (data) {
+				var $data =  JSON.parse(data);
+				$("form")
+				$('.error').text('');
+				if ($data.result == "success") {
+ 			// скрываем форму обратной связи
+ 			$('form').hide();
+			// удаляем у элемента, имеющего id=successMessage, класс hidden
+			$('#successMessage').removeClass('hidden');
+		}else {
+            // если сервер вернул ответ error, то делаем следующее...
+            $('error').text('There were errors while sending the form to the server.');
+          }
+          
+        },
+        error: function (error) {
+        	
+        	$('.error').text('An error has occurred  when sending data.');
+        }     
+      });
+		setTimeout(function(){
+			$(':input', 'form')
+			.not(':button, :submit, :reset, :hidden')
+			.val('')
+			.removeAttr('checked');
+		}, 3000);
 	});
 });
